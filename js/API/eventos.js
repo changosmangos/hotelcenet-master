ac2ab87f4e5c5763a1e52fdb5086db29bc0f00b6
Carlos Alberto Rodriguez Garcia <tacubayo8@gmail.com>
2013-06-15 19:05:53 -0500

    muajajaja xD
    puuuf

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -1,15 +1,25 @@
 // Eventos
 $(document).ready(function(e){
-  $("#regSend").tap (function() {
-	  if($("#regNom").val()=! ""&& $("#regTel").val()!= "" && $("regMail").val()!= "") {
-		  var nom = $("#regNom").val();
-		  var tel = $("#regTek").val();
-		  var mail = $("#regMail").val();
-		  alert (nom + "\n" + tel + "\n" + mail );
-	  }else{
-		alert("Se necesitan llenar los campos obligatoriamente");  
+	document.addEventListener("deviceready", function(){ 
+			if(!isLogin())
+			window.location.href = "#login"; 
+		  $("#regSend").tap (function() {
+			  if($("#regNom").val()=! ""&& $("#regTel").val()!= "" && $("regMail").val()!= "") {
+				  var nom = $("#regNom").val();
+				  var tel = $("#regTek").val();
+				  var mail = $("#regMail").val();
+				  navigator.notification.alert (nom + "\n" + tel + "\n" + mail, null, "HOTEL", "Aceptar" );
+				  
+			  }else{
+				alert("Se necesitan llenar los campos obligatoriamente", null, "HOTEL", "Ok");
+				  
+			  }
 		  
-	  }
-  
-  });
+		  });
+	}, false);
+	
 });
+
+function isLogin(){
+	return false;
+}
